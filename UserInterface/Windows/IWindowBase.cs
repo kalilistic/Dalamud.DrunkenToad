9dc72e87c16a3ceac73a9a58403ad93653eae77c
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-01-25 20:19:49 -0500

    improve window base

---
+++ UserInterface/Windows/IWindowBase.cs
@@ -0,0 +1,12 @@
+﻿namespace DalamudPluginCommon
+{
+	public interface IWindowBase
+	{
+		float Scale { get; }
+		bool IsVisible { get; }
+		void DrawView();
+		void ToggleView();
+		void ShowView();
+		void HideView();
+	}
+}
--- UserInterface/Windows/WindowBase.cs
+++ UserInterface/Windows/WindowBase.cs
@@ -6,11 +6,28 @@ using ImGuiNET;
 
 namespace DalamudPluginCommon
 {
-	public abstract class WindowBase
+	public abstract class WindowBase : IWindowBase
 	{
 		public readonly List<Vector4> ColorPalette = new List<Vector4>();
 		public CustomWidgets CustomWidgets = new CustomWidgets();
-		public bool IsVisible = false;
+		public bool IsVisible { get; protected set; }
 		public float Scale => ImGui.GetIO().FontGlobalScale;
+
+		public abstract void DrawView();
+
+		public void ToggleView()
+		{
+			IsVisible = !IsVisible;
+		}
+
+		public void ShowView()
+		{
+			IsVisible = true;
+		}
+
+		public void HideView()
+		{
+			IsVisible = false;
+		}
 	}
 }
