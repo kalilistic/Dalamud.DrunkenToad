f60223c7e72840499e11811658d6513a7f0516c5
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-01-25 22:07:34 -0500

    add short timespan ext

--- Extension/LongExtensions.cs
+++ Extension/LongExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// ReSharper disable ConvertIfStatementToReturnStatement
+
+using System;
 using System.Collections.Generic;
 
 namespace DalamudPluginCommon
@@ -13,14 +15,13 @@ namespace DalamudPluginCommon
 
 		public static string ToTimeSpan(this long value)
 		{
-			var timeSpan = ConvertToTimeSpan(DateUtil.CurrentTime() - value);
+			var timeSpan = ConvertToShortTimeSpan(DateUtil.CurrentTime() - value);
 			return string.IsNullOrEmpty(timeSpan) ? "Now" : timeSpan + " ago";
 		}
 
 		public static string ToDuration(this long value)
 		{
 			var timeSpan = ConvertToTimeSpan(value);
-			var result = string.Join(" ", timeSpan);
 			return string.IsNullOrEmpty(timeSpan) ? "< 1m" : timeSpan;
 		}
 
@@ -38,6 +39,15 @@ namespace DalamudPluginCommon
 			Add(t.Hours, "h");
 			Add(t.Minutes, "m");
 			return string.Join(" ", parts);
+		}
+
+		private static string ConvertToShortTimeSpan(long value)
+		{
+			var timeSpan = TimeSpan.FromMilliseconds(value);
+			if (timeSpan.Days > 0) return timeSpan.Days + "d";
+			if (timeSpan.Hours > 0) return timeSpan.Hours + "h";
+			if (timeSpan.Minutes > 0) return timeSpan.Minutes + "m";
+			return string.Empty;
 		}
 	}
 }
