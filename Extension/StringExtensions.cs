fdff6ad3adf5731515c92f444337c40d02089f1b
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-03-27 12:10:19 -0400

    fix sanitize method for de

--- Extension/StringExtensions.cs
+++ Extension/StringExtensions.cs
@@ -12,7 +12,8 @@ namespace DalamudPluginCommon
         private static readonly List<KeyValuePair<string, string>> SanitizeList = new List<KeyValuePair<string, string>>
         {
             new KeyValuePair<string, string>("the ", Empty),
-            new KeyValuePair<string, string>("\u0002\u001F\u0001\u0003", "-"),
+            new KeyValuePair<string, string>("\u0002\u001F\u0001\u0003", Empty),
+            new KeyValuePair<string, string>("\u0002\u0016\u0001\u0003", Empty),
             new KeyValuePair<string, string>("\u0002\u001a\u0002\u0002\u0003", Empty),
             new KeyValuePair<string, string>("\u0002\u001a\u0002\u0001\u0003", Empty)
         };
