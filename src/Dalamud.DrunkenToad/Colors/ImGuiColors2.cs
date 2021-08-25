2da7f029567e907e18ae113b1bc22bbbd218e421
kalilistic <35899782+kalilistic@users.noreply.github.com>
2021-08-25 16:39:45 -0400

    feat: add more colors

--- src/Dalamud.DrunkenToad/Colors/ImGuiColors2.cs
+++ src/Dalamud.DrunkenToad/Colors/ImGuiColors2.cs
@@ -16,5 +16,40 @@ namespace Dalamud.DrunkenToad
         /// Gets violet color.
         /// </summary>
         public static Vector4 ToadViolet { get; } = new (0.770f, 0.700f, 0.965f, 1.000f);
+
+        /// <summary>
+        /// Gets FFLogs grey.
+        /// </summary>
+        public static Vector4 Common { get; } = new (0.4f, 0.4f, 0.4f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs green.
+        /// </summary>
+        public static Vector4 Uncommon { get; } = new (0.117f, 1f, 0f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs blue.
+        /// </summary>
+        public static Vector4 Rare { get; } = new (0f, 0.439f, 1f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs purple.
+        /// </summary>
+        public static Vector4 Epic { get; } = new (0.639f, 0.207f, 0.933f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs orange.
+        /// </summary>
+        public static Vector4 Legendary { get; } = new (1f, 0.501f, 0f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs pink.
+        /// </summary>
+        public static Vector4 Astounding { get; } = new (0.886f, 0.407f, 0.658f, 1f);
+
+        /// <summary>
+        /// Gets FFLogs yellow.
+        /// </summary>
+        public static Vector4 Artifact { get; } = new (0.898f, 0.8f, 0.501f, 1f);
     }
 }
---
+++ src/Dalamud.DrunkenToad/Util/ImGuiColorUtil.cs
@@ -0,0 +1,32 @@
+using System.Numerics;
+
+using Dalamud.Interface.Colors;
+
+namespace Dalamud.DrunkenToad
+{
+    /// <summary>
+    /// Utility functions for working with imgui colors.
+    /// </summary>
+    public static class ImGuiColorUtil
+    {
+        /// <summary>
+        /// Get color by number (follows FFLogs ranks).
+        /// </summary>
+        /// <param name="num">number to use to determine color.</param>
+        /// <returns>color.</returns>
+        public static Vector4 GetColorByNumber(uint num)
+        {
+            return num switch
+            {
+                0 => ImGuiColors.White,
+                < 25 => ImGuiColors2.Common,
+                >= 25 and <= 49 => ImGuiColors2.Uncommon,
+                >= 50 and <= 74 => ImGuiColors2.Rare,
+                >= 75 and <= 94 => ImGuiColors2.Epic,
+                >= 95 and <= 98 => ImGuiColors2.Legendary,
+                99 => ImGuiColors2.Astounding,
+                _ => ImGuiColors2.Artifact,
+            };
+        }
+    }
+}
