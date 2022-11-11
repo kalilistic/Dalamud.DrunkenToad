844eb657aeb24ea6d43aca986de71cf079d6b412
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-11-10 19:52:55 -0500

    feat: add AddOn util and interface ext

---
+++ Dalamud.DrunkenToad/Extension/PluginInterfaceExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Dalamud.Plugin;
+
+namespace Dalamud.DrunkenToad;
+
+/// <summary>
+/// Dalamud game object extensions.
+/// </summary>
+public static class PluginInterfaceExtensions
+{
+    /// <summary>
+    /// Sanitize string to remove unprintable characters (short hand method).
+    /// </summary>
+    /// <param name="value">dalamud plugin interface with sanitizer initialized.</param>
+    /// <param name="str">string to sanitize.</param>
+    /// <returns>Indicator if player character is valid.</returns>
+    public static string Sanitize(this DalamudPluginInterface value, string str)
+    {
+        if (string.IsNullOrEmpty(str)) return str;
+        return value.Sanitizer.Sanitize(str);
+    }
+
+    /// <summary>
+    /// Sanitize dalamud se string to remove unprintable characters (short hand method).
+    /// </summary>
+    /// <param name="value">dalamud plugin interface with sanitizer initialized.</param>
+    /// <param name="str">string to sanitize.</param>
+    /// <returns>Indicator if player character is valid.</returns>
+    public static string Sanitize(this DalamudPluginInterface value, Game.Text.SeStringHandling.SeString str)
+    {
+        return Sanitize(value, str.ToString());
+    }
+
+    /// <summary>
+    /// Sanitize lumina se string to remove unprintable characters (short hand method).
+    /// </summary>
+    /// <param name="value">dalamud plugin interface with sanitizer initialized.</param>
+    /// <param name="str">string to sanitize.</param>
+    /// <returns>Indicator if player character is valid.</returns>
+    public static string Sanitize(this DalamudPluginInterface value, Lumina.Text.SeString str)
+    {
+        return Sanitize(value, str.ToString());
+    }
+}
---
+++ Dalamud.DrunkenToad/Util/AddOnUtil.cs
@@ -0,0 +1,68 @@
+﻿using FFXIVClientStructs.FFXIV.Component.GUI;
+
+namespace Dalamud.DrunkenToad;
+
+/// <summary>
+/// More easily work with GUI AddOns.
+/// </summary>
+public unsafe class AddOnHelper
+{
+    /// <summary>
+    /// Check if addon is ready.
+    /// </summary>
+    /// <param name="addon">addon.</param>
+    /// <returns>indicator if addon is ready.</returns>
+    public static bool IsAddonReady(AtkComponentNode* addon)
+    {
+        return addon != null && addon->AtkResNode.IsVisible && addon->Component->UldManager.LoadedState == AtkLoadState.Loaded;
+    }
+
+    /// <summary>
+    /// Check if addon is ready.
+    /// </summary>
+    /// <param name="addon">addon.</param>
+    /// <returns>indicator if addon is ready.</returns>
+    public static bool IsAddonReady(AtkTextNode* addon)
+    {
+        return addon != null && addon->AtkResNode.IsVisible;
+    }
+
+    /// <summary>
+    /// Check if unit base is ready.
+    /// </summary>
+    /// <param name="unitBase">unit base.</param>
+    /// <returns>indicator if unit base is ready.</returns>
+    public static bool IsUnitBaseReady(AtkUnitBase* unitBase)
+    {
+        return unitBase != null && unitBase->IsVisible && unitBase->UldManager.LoadedState == AtkLoadState.Loaded;
+    }
+
+    /// <summary>
+    /// Extract text from component node.
+    /// </summary>
+    /// <param name="unitBase">unit base.</param>
+    /// <param name="textNodeId">text node id.</param>
+    /// <param name="componentNodeId">component node id.</param>
+    /// <returns>node text.</returns>
+    public static string GetNodeTextFromComponentNode(AtkUnitBase* unitBase, uint textNodeId, uint componentNodeId)
+    {
+        var componentNode = (AtkComponentNode*)unitBase->UldManager.SearchNodeById(componentNodeId);
+        if (!IsAddonReady(componentNode)) return string.Empty;
+        var textNode = (AtkTextNode*)componentNode->Component->UldManager.SearchNodeById(textNodeId);
+        if (!IsAddonReady(textNode)) return string.Empty;
+        return DalamudContext.PluginInterface.Sanitize(textNode->NodeText.ToString());
+    }
+
+    /// <summary>
+    /// Extract text from component node.
+    /// </summary>
+    /// <param name="unitBase">unit base.</param>
+    /// <param name="nodeId">node id.</param>
+    /// <returns>node text.</returns>
+    public static string GetNodeText(AtkUnitBase* unitBase, uint nodeId)
+    {
+        var textNode = (AtkTextNode*)unitBase->UldManager.SearchNodeById(nodeId);
+        if (!IsAddonReady(textNode)) return string.Empty;
+        return DalamudContext.PluginInterface.Sanitize(textNode->NodeText.ToString());
+    }
+}
