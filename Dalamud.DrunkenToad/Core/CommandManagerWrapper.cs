6ce7662c1da082a566532ca1d1c185f756412f8c
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-11-19 11:10:55 -0500

    refactor: convert command code into wrapper class

---
+++ Dalamud.DrunkenToad/Core/CommandManagerWrapper.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Dalamud.Game.Command;
+
+namespace Dalamud.DrunkenToad.Core;
+
+/// <summary>
+/// Dalamud command manager wrapper to provide state.
+/// https://github.com/goatcorp/Dalamud/blob/master/Dalamud/Game/Command/CommandManager.cs.
+/// </summary>
+public class CommandManagerWrapper
+{
+    private readonly List<string> commandRegistry = new ();
+    private readonly CommandManager commandManager;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CommandManagerWrapper"/> class.
+    /// </summary>
+    /// <param name="commandManager">dalamud command manager.</param>
+    public CommandManagerWrapper(CommandManager commandManager)
+    {
+        this.commandManager = commandManager;
+    }
+
+    /// <summary>
+    /// Add a command handler, which you can use to add your own custom commands to the in-game chat.
+    /// </summary>
+    /// <param name="command">The command to register.</param>
+    /// <param name="handler">The action to invoke on command use.</param>
+    /// <returns>If adding was successful.</returns>
+    public bool RegisterCommand(string command, CommandInfo.HandlerDelegate handler)
+    {
+        var addedSuccessfully = this.commandManager.AddHandler(command, new CommandInfo(handler)
+        {
+            ShowInHelp = false,
+        });
+        if (addedSuccessfully) this.commandRegistry.Add(command);
+        return addedSuccessfully;
+    }
+
+    /// <summary>
+    /// Add a command handler, which you can use to add your own custom commands to the in-game chat.
+    /// </summary>
+    /// <param name="command">The command to register.</param>
+    /// <param name="helpMessage">The help text to show in installer.</param>
+    /// <param name="handler">The action to invoke on command use.</param>
+    /// <returns>If adding was successful.</returns>
+    public bool RegisterCommand(string command, string helpMessage, CommandInfo.HandlerDelegate handler)
+    {
+        var addedSuccessfully = this.commandManager.AddHandler(command, new CommandInfo(handler)
+        {
+            HelpMessage = helpMessage,
+            ShowInHelp = true,
+        });
+        if (addedSuccessfully) this.commandRegistry.Add(command);
+        return addedSuccessfully;
+    }
+
+    /// <summary>
+    /// Gets a read-only list of all registered commands.
+    /// </summary>
+    /// <returns>list of registered commands.</returns>
+    public ReadOnlyDictionary<string, CommandInfo> Commands()
+    {
+        return this.commandManager.Commands;
+    }
+
+    /// <summary>
+    /// Dispose wrapper.
+    /// </summary>
+    public void Dispose()
+    {
+        foreach (var command in this.commandRegistry)
+        {
+            this.commandManager.RemoveHandler(command);
+        }
+    }
+}
--- Dalamud.DrunkenToad/Core/DalamudContext.cs
+++ Dalamud.DrunkenToad/Core/DalamudContext.cs
@@ -30,11 +30,17 @@ namespace Dalamud.DrunkenToad.Core;
 /// </summary>
 public class DalamudContext
 {
+    /// <summary>
+    /// Initialize dalamud context with dalamud and custom services.
+    /// </summary>
+    /// <param name="pluginInterface">dalamud plugin interface.</param>
+    /// <returns>indicator if initialized successfully.</returns>
     public static bool Initialize(DalamudPluginInterface pluginInterface)
     {
         try
         {
             pluginInterface.Create<DalamudContext>();
+            Commands = new CommandManagerWrapper(CommandManager);
             Localization = new Loc.Localization(pluginInterface);
             Configuration = new DalamudConfiguration();
             return true;
@@ -45,33 +51,153 @@ public class DalamudContext
         }
     }
 
-    [PluginService] public static DataManager GameData { get; private set; } = null!;
-    [PluginService] public static AetheryteList Aetherytes { get; private set; } = null!;
-    [PluginService] public static BuddyList Buddies { get; private set; } = null!;
-    [PluginService] public static Condition Conditions { get; private set; } = null!;
-    [PluginService] public static FateTable Fates { get; private set; } = null!;
-    [PluginService] public static GamepadState GamepadState { get; private set; } = null!;
-    [PluginService] public static JobGauges Gauges { get; private set; } = null!;
-    [PluginService] public static KeyState KeyState { get; private set; } = null!;
-    [PluginService] public static ObjectTable Objects { get; private set; } = null!;
-    [PluginService] public static TargetManager Targets { get; private set; } = null!;
-    [PluginService] public static PartyList Party { get; private set; } = null!;
-    [PluginService] public static ClientState ClientState { get; private set; } = null!;
-    [PluginService] public static CommandManager Commands { get; private set; } = null!;
-    [PluginService] public static DtrBar DtrBar { get; private set; } = null!;
-    [PluginService] public static FlyTextGui FlyTexts { get; private set; } = null!;
-    [PluginService] public static PartyFinderGui PartyFinder { get; private set; } = null!;
-    [PluginService] public static ToastGui Toasts { get; private set; } = null!;
-    [PluginService] public static ChatGui ChatGui { get; private set; } = null!;
-    [PluginService] public static GameGui GameGui { get; private set; } = null!;
-    [PluginService] public static LibcFunction LibC { get; private set; } = null!;
-    [PluginService] public static GameNetwork Network { get; private set; } = null!;
-    [PluginService] public static ChatHandlers ChatHandlers { get; private set; } = null!;
-    [PluginService] public static Framework Framework { get; private set; } = null!;
-    [PluginService] public static SigScanner SigScanner { get; private set; } = null!;
-    [PluginService] public static TitleScreenMenu TitleScreenMenu { get; private set; } = null!;
-    [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+    /// <summary>
+    /// Dispose custom services.
+    /// </summary>
+    public static void Dispose()
+    {
+        Commands.Dispose();
+        Localization.Dispose();
+    }
 
+    /// <summary>
+    /// Gets interface for using Dalamud DalamudConfiguration.
+    /// </summary>
     public static DalamudConfiguration Configuration { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets localization class.
+    /// </summary>
     public static Loc.Localization Localization { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets dalamud command manager wrapper to provide state.
+    /// https://github.com/goatcorp/Dalamud/blob/master/Dalamud/Game/Command/CommandManager.cs.
+    /// </summary>
+    public static CommandManagerWrapper Commands { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class providing game data for dalamud and plugins.
+    /// </summary>
+    [PluginService] public static DataManager GameData { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets this collection represents the list of available Aetherytes in the Teleport window.
+    /// </summary>
+    [PluginService] public static AetheryteList Aetherytes { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets collection representing the buddies present in your squadron or trust party.
+    /// </summary>
+    [PluginService] public static BuddyList Buddies { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class providing access to conditions (generally player state).
+    /// </summary>
+    [PluginService] public static Condition Conditions { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets collection representing the currently available Fate events.
+    /// </summary>
+    [PluginService] public static FateTable Fates { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class providing access to gamepad state.
+    /// Will block game's gamepad input if <see cref="F:ImGuiNET.ImGuiConfigFlags.NavEnableGamepad" /> is set.
+    /// </summary>
+    [PluginService] public static GamepadState GamepadState { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class providing access to Job gauge data.
+    /// </summary>
+    [PluginService] public static JobGauges JobGauges { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets wrapper around the game keystate buffer, which contains the pressed state for all keyboard keys, indexed by virtual vkCode.
+    /// </summary>
+    [PluginService] public static KeyState KeyState { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets collection representing the currently spawned FFXIV game objects.
+    /// </summary>
+    [PluginService] public static ObjectTable Objects { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets collection representing targets for the player.
+    /// </summary>
+    [PluginService] public static TargetManager Targets { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets collection representing the actors present in your party or alliance.
+    /// </summary>
+    [PluginService] public static PartyList Party { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets this class represents the state of the game client at the time of access.
+    /// </summary>
+    [PluginService] public static ClientState ClientState { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class used to interface with the server info bar.
+    /// </summary>
+    [PluginService] public static DtrBar DtrBar { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class facilitating interacting with and creating native in-game "fly text".
+    /// </summary>
+    [PluginService] public static FlyTextGui FlyTexts { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class handling interacting the native PartyFinder window.
+    /// </summary>
+    [PluginService] public static PartyFinderGui PartyFinder { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class facilitating interacting with and creating native toast windows.
+    /// </summary>
+    [PluginService] public static ToastGui ToastGui { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class handling interacting with the native chat UI.
+    /// </summary>
+    [PluginService] public static ChatGui ChatGui { get; private set; } = null!;
+
+    /// <summary>Gets a class handling many aspects of the in-game UI.</summary>
+    [PluginService] public static GameGui GameGui { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class handling creating c strings utilizing native game methods.
+    /// </summary>
+    [PluginService] public static LibcFunction LibC { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class handling interacting with game network events.
+    /// </summary>
+    [PluginService] public static GameNetwork Network { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets chat events and public helper functions.
+    /// </summary>
+    [PluginService] public static ChatHandlers ChatHandlers { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class representing the Framework of the native game client and grants access to various subsystems.
+    /// </summary>
+    [PluginService] public static Framework Framework { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets a SigScanner facilitates searching for memory signatures in a given ProcessModule.
+    /// </summary>
+    [PluginService] public static SigScanner SigScanner { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class responsible for managing elements in the title screen menu.
+    /// </summary>
+    [PluginService] public static TitleScreenMenu TitleScreenMenu { get; private set; } = null!;
+
+    /// <summary>
+    /// Gets class acting as an interface to various objects needed to interact with Dalamud and the game.
+    /// </summary>
+    [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
+    [PluginService] private static CommandManager CommandManager { get; set; } = null!;
 }
