a16941ac6461bf316b4b9cf898e1b540a77098cb
kalilistic <35899782+kalilistic@users.noreply.github.com>
2022-11-19 10:21:43 -0500

    refactor: move nswag into subdir

--- Dalamud.DrunkenToad/WebAPI/KalAPI/KalAPI.cs
+++ Dalamud.DrunkenToad/WebAPI/KalAPI/KalAPI.cs
@@ -13,7 +13,7 @@
 #pragma warning disable 3016 // Disable "CS3016 Arrays as attribute arguments is not CLS-compliant"
 #pragma warning disable 8603 // Disable "CS8603 Possible null reference return"
 
-namespace Dalamud.DrunkenToad.KalAPI.WebAPI
+namespace Dalamud.DrunkenToad.WebAPI.KalAPI
 {
     using System = global::System;
 
