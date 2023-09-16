46c5f5d672bb8b3013e68b974fcaaa48f4bf9ab1
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-09-16 17:27:56 -0400

    refactor: add desc param to perf loggers

--- Dalamud.DrunkenToad/Helpers/PerformanceLogger.cs
+++ Dalamud.DrunkenToad/Helpers/PerformanceLogger.cs
@@ -14,25 +14,31 @@ public static class PerformanceHelper
     /// Executes the provided action and logs the time taken for its execution.
     /// </summary>
     /// <param name="action">The action to be executed.</param>
-    public static void ExecuteAndLog(Action action)
+    /// <param name="description">The description.</param>
+    public static void ExecuteAndLog(Action action, string description = "")
     {
         var stopwatch = Stopwatch.StartNew();
         action();
         stopwatch.Stop();
-        PluginLog.Log($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+        PluginLog.Log(string.IsNullOrEmpty(description)
+            ? $"Time taken: {stopwatch.ElapsedMilliseconds} ms"
+            : $"[{description}] Time taken: {stopwatch.ElapsedMilliseconds} ms");
     }
 
     /// <summary>
     /// Executes the provided asynchronous action and logs the time taken for its execution.
     /// </summary>
     /// <param name="asyncAction">The asynchronous action to be executed.</param>
+    /// <param name="description">The description.</param>
     /// <returns>The result of the function execution.</returns>
-    public static async Task ExecuteAndLogAsync(Func<Task> asyncAction)
+    public static async Task ExecuteAndLogAsync(Func<Task> asyncAction, string description = "")
     {
         var stopwatch = Stopwatch.StartNew();
         await asyncAction();
         stopwatch.Stop();
-        PluginLog.Log($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+        PluginLog.Log(string.IsNullOrEmpty(description)
+            ? $"Time taken: {stopwatch.ElapsedMilliseconds} ms"
+            : $"[{description}] Time taken: {stopwatch.ElapsedMilliseconds} ms");
     }
 
     /// <summary>
@@ -40,13 +46,17 @@ public static class PerformanceHelper
     /// </summary>
     /// <typeparam name="TResult">The type of the result.</typeparam>
     /// <param name="func">The function to be executed.</param>
+    /// <param name="description">The description.</param>
     /// <returns>The result of the function execution.</returns>
-    public static TResult ExecuteAndLog<TResult>(Func<TResult> func)
+    public static TResult ExecuteAndLog<TResult>(Func<TResult> func, string description = "")
     {
         var stopwatch = Stopwatch.StartNew();
         var result = func();
         stopwatch.Stop();
-        PluginLog.Log($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+        PluginLog.Log(string.IsNullOrEmpty(description)
+            ? $"Time taken: {stopwatch.ElapsedMilliseconds} ms"
+            : $"[{description}] Time taken: {stopwatch.ElapsedMilliseconds} ms");
+
         return result;
     }
 
@@ -55,13 +65,16 @@ public static class PerformanceHelper
     /// </summary>
     /// <typeparam name="TResult">The type of the result.</typeparam>
     /// <param name="asyncFunc">The asynchronous function to be executed.</param>
+    /// <param name="description">The description.</param>
     /// <returns>The result of the function execution.</returns>
-    public static async Task<TResult> ExecuteAndLogAsync<TResult>(Func<Task<TResult>> asyncFunc)
+    public static async Task<TResult> ExecuteAndLogAsync<TResult>(Func<Task<TResult>> asyncFunc, string description = "")
     {
         var stopwatch = Stopwatch.StartNew();
         var result = await asyncFunc();
         stopwatch.Stop();
-        PluginLog.Log($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+        PluginLog.Log(string.IsNullOrEmpty(description)
+            ? $"Time taken: {stopwatch.ElapsedMilliseconds} ms"
+            : $"[{description}] Time taken: {stopwatch.ElapsedMilliseconds} ms");
         return result;
     }
 }
