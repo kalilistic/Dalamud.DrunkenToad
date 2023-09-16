5cb101120e17f1a09e441078f447205b8e61eb15
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-09-15 22:56:39 -0400

    refactor: migration window

--- Dalamud.DrunkenToad/Gui/Windows/MigrationWindow.cs
+++ Dalamud.DrunkenToad/Gui/Windows/MigrationWindow.cs
@@ -4,7 +4,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Numerics;
 using ImGuiNET;
 using Interface;
 using Interface.Colors;
@@ -15,25 +14,14 @@ using Plugin;
 /// </summary>
 public class MigrationWindow : SimpleWindow
 {
-    /// <summary>
-    /// Gets a value for migration errors.
-    /// </summary>
     public readonly ConcurrentQueue<string> ErrorQueue = new ();
-
-    /// <summary>
-    /// Gets a value for migration steps.
-    /// </summary>
     public readonly ConcurrentQueue<string> StepQueue = new ();
-
     private readonly List<string> errorMessages = new ();
     private readonly List<string> steps = new ();
     private readonly Stopwatch stopwatch;
     private bool isMigrationFinished;
+    private int previousStepCount;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MigrationWindow" /> class.
-    /// </summary>
-    /// <param name="pluginInterface">dalamud plugin interface.</param>
     public MigrationWindow(DalamudPluginInterface pluginInterface)
         : base(pluginInterface)
     {
@@ -41,16 +29,12 @@ public class MigrationWindow : SimpleWindow
         this.stopwatch.Start();
     }
 
-    /// <summary>
-    /// End migration to show close button and stop timer.
-    /// </summary>
     public void StopMigration()
     {
         this.stopwatch.Stop();
         this.isMigrationFinished = true;
     }
 
-    /// <inheritdoc />
     protected override void Draw()
     {
         if (!this.isOpen)
@@ -58,17 +42,62 @@ public class MigrationWindow : SimpleWindow
             return;
         }
 
-        ImGui.SetNextWindowSize(ImGuiHelpers.ScaledVector2(700f, 500f), ImGuiCond.Appearing);
-        ImGui.Begin($"{this.pluginInterface.InternalName}##Migration", ref this.isOpen);
+        ImGui.SetNextWindowSize(ImGuiHelpers.ScaledVector2(600f, 400f), ImGuiCond.Appearing);
+        ImGui.Begin($"{this.pluginInterface.InternalName}##Migration");
 
-        // Header Section
-        ImGui.TextColored(ImGuiColors.DalamudViolet, $"{this.pluginInterface.InternalName} Migration");
-        ImGui.BulletText($"We need to migrate your {this.pluginInterface.InternalName} data to a new database.");
-        ImGui.BulletText("Do not close the window until the migration is complete.");
-        ImGui.BulletText("Report any issues on discord or through the plugin installer.");
+        this.DrawHeaderSection();
+        this.DrawStepsSection();
+        this.DrawButtons();
+        this.DrawMigrationTimer();
+
+        ImGui.End();
+    }
+
+    private void DrawMigrationTimer()
+    {
+        ImGui.Text($"Elapsed Time: {this.stopwatch.Elapsed:mm\\:ss\\.fff}");
+        ImGuiHelpers.ScaledDummy(3f);
+    }
+
+    private void DrawButtons()
+    {
+        ImGuiHelpers.ScaledDummy(4f);
+        var isButtonDisabled = !this.isMigrationFinished;
+        ImGui.BeginDisabled(isButtonDisabled);
+        if (ImGui.Button("Close Window"))
+        {
+            this.isOpen = false;
+        }
+
+        ImGui.EndDisabled();
+
+        if (this.errorMessages.Count > 0)
+        {
+            ImGui.SameLine();
+            ImGui.BeginDisabled(isButtonDisabled);
+            if (ImGui.Button("Copy Errors to Clipboard"))
+            {
+                ImGui.SetClipboardText(string.Join(
+                    "\n",
+                    new[] { $"{this.pluginInterface.InternalName} Migration Error Log" }.Concat(this.errorMessages)));
+            }
+
+            ImGui.EndDisabled();
+        }
+
+        ImGuiHelpers.ScaledDummy(1f);
+    }
+
+    private void DrawHeaderSection()
+    {
+        ImGui.TextColored(ImGuiColors.DalamudViolet, "Database Migration");
+        ImGui.BulletText("Do not interrupt the upgrade..");
+        ImGui.BulletText("Report any issues on Discord or GitHub.");
         ImGui.Spacing();
+    }
 
-        // Steps Section
+    private void DrawStepsSection()
+    {
         while (!this.StepQueue.IsEmpty)
         {
             if (this.StepQueue.TryDequeue(out var msg) && !string.IsNullOrEmpty(msg))
@@ -77,19 +106,10 @@ public class MigrationWindow : SimpleWindow
             }
         }
 
-        ImGuiHelpers.ScaledDummy(2f);
+        ImGuiHelpers.ScaledDummy(1f);
         ImGui.TextColored(ImGuiColors.DalamudViolet, "Migration Progress");
 
-        var totalHeight = 0f;
-        foreach (var step in this.steps)
-        {
-            var textSize = ImGui.CalcTextSize(step);
-            totalHeight += textSize.Y + ImGui.GetTextLineHeightWithSpacing();
-        }
-
-        var childSize = new Vector2(-1, totalHeight - 50 * ImGuiHelpers.GlobalScale);
-        ImGui.BeginChild("##stepsChildWindow", childSize, true);
-
+        ImGui.BeginChild("Messages", ImGuiHelpers.ScaledVector2(0f, 100f), true);
         foreach (var step in this.steps)
         {
             ImGui.Indent(1f);
@@ -103,7 +123,14 @@ public class MigrationWindow : SimpleWindow
             ImGui.Text(step);
         }
 
-        // Add Error Message
+        while (!this.ErrorQueue.IsEmpty)
+        {
+            if (this.ErrorQueue.TryDequeue(out var msg) && !string.IsNullOrEmpty(msg))
+            {
+                this.errorMessages.Add(msg);
+            }
+        }
+
         if (this.errorMessages.Count > 0)
         {
             ImGui.Indent(3.5f);
@@ -114,42 +141,15 @@ public class MigrationWindow : SimpleWindow
             ImGui.PopFont();
             ImGui.Unindent(3.5f);
             ImGui.SameLine();
-            ImGui.Text(" Something went wrong... failed to migrate!");
+            ImGui.Text(" Migration failed! Report this on Discord or GitHub.");
+        }
+
+        if (this.previousStepCount < this.steps.Count)
+        {
+            ImGui.SetScrollHereY(1.0f);
+            this.previousStepCount = this.steps.Count;
         }
 
         ImGui.EndChild();
-
-        while (!this.ErrorQueue.IsEmpty)
-        {
-            if (this.ErrorQueue.TryDequeue(out var msg) && !string.IsNullOrEmpty(msg))
-            {
-                this.errorMessages.Add(msg);
-            }
-        }
-
-        // Footer Section
-        ImGuiHelpers.ScaledDummy(10f);
-        ImGui.Text($"Elapsed Time: {this.stopwatch.Elapsed:mm\\:ss\\.fff}");
-
-        if (this.isMigrationFinished)
-        {
-            if (ImGui.Button("Close Window"))
-            {
-                this.isOpen = false;
-            }
-
-            if (this.errorMessages.Count > 0)
-            {
-                ImGui.SameLine();
-                if (ImGui.Button("Copy Errors to Clipboard"))
-                {
-                    ImGui.SetClipboardText(string.Join(
-                        "\n",
-                        new[] { $"{this.pluginInterface.InternalName} Migration Error Log" }.Concat(this.errorMessages)));
-                }
-            }
-        }
-
-        ImGui.End();
     }
 }
