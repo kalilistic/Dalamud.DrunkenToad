210c72ed25b00d824e2308205764a5f9cf1b46fc
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-10-07 19:21:28 -0400

    feat: change helper methods to interface ext

--- Dalamud.DrunkenToad/Extensions/PluginInterfaceExtensions.cs
+++ Dalamud.DrunkenToad/Extensions/PluginInterfaceExtensions.cs
@@ -1,5 +1,7 @@
 ﻿namespace Dalamud.DrunkenToad.Extensions;
 
+using System;
+using System.IO;
 using Game.Text.SeStringHandling;
 using Plugin;
 
@@ -39,4 +41,17 @@ public static class PluginInterfaceExtensions
     /// <param name="str">string to sanitize.</param>
     /// <returns>Indicator if player character is valid.</returns>
     public static string Sanitize(this DalamudPluginInterface value, Lumina.Text.SeString str) => Sanitize(value, str.ToString());
+
+    /// <summary>
+    /// Get the plugin backup directory.
+    /// </summary>
+    /// <param name="value">dalamud plugin interface.</param>
+    /// <returns>Plugin backup directory.</returns>
+    public static string PluginBackupDirectory(this DalamudPluginInterface value)
+    {
+        var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        var backupsDir = Path.Combine(appDataDir, "XIVLauncher", "pluginBackups", value.InternalName);
+        Directory.CreateDirectory(backupsDir);
+        return backupsDir;
+    }
 }
