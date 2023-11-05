050317f243a079676e6014660f5068437e4c48d0
kalilistic <35899782+kalilistic@users.noreply.github.com>
2023-11-05 09:46:28 -0500

    feat: get local player

---
+++ Dalamud.DrunkenToad/Core/Models/ToadLocalPlayer.cs
@@ -0,0 +1,38 @@
+﻿namespace Dalamud.DrunkenToad.Core.Models;
+
+/// <summary>
+/// Subset of key properties from local player for eventing.
+/// </summary>
+public class ToadLocalPlayer
+{
+    /// <summary>
+    /// The content ID of the local character.
+    /// </summary>
+    public ulong ContentId;
+
+    /// <summary>
+    /// Player Name.
+    /// </summary>
+    public string Name = string.Empty;
+
+    /// <summary>
+    /// Player HomeWorld ID.
+    /// </summary>
+    public uint HomeWorld;
+
+    /// <summary>
+    /// Player Customize Array.
+    /// </summary>
+    public byte[]? Customize;
+
+    /// <summary>
+    /// Player Company Tag.
+    /// </summary>
+    public string CompanyTag = string.Empty;
+
+    /// <summary>
+    /// Validate if local player is valid.
+    /// </summary>
+    /// <returns>Indicator if local player is valid.</returns>
+    public bool IsValid() => this.ContentId != 0 && this.Name != string.Empty && this.HomeWorld != 0;
+}
